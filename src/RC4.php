6fc586ff719d2ed452cba52712226102bcc804ce
JonChou <ilorn.mc@gmail.com>
2015-02-19 21:44:11 +0800

    Code refactoring

--- src/RC4.php
+++ src/RC4.php
@@ -2,7 +2,8 @@
 
 class RC4 
 {
-    const ENCRYPT_MODE_UPDATE = true;
+    const ENCRYPT_MODE_UPDATE = 1;
+    const ENCRYPT_MODE_NORMAL = 0;
 
     protected $password = null;
     protected $mode = false;
@@ -10,7 +11,7 @@ class RC4
     protected $i = 0;
     protected $j = 0;
 
-    public function __construct($password, $mode = false)
+    public function __construct($password, $mode = 0)
     {
         $this->password = $password;
         $this->mode = $mode;
@@ -32,14 +33,14 @@ class RC4
         $this->i = $this->j = 0;
     }
 
-    public function encrypt($data) 
+    public function encrypt($plaintext) 
     {
-        $dataLength = strlen($data);
-        $cipher = '';
-        if ($this->mode !== static::ENCRYPT_MODE_UPDATE) {
+        $plaintextLength = strlen($plaintext);
+        $ciphertext = '';
+        if ($this->mode === static::ENCRYPT_MODE_NORMAL) {
             $this->initCipher();
         }
-        for ($n = 0; $n < $dataLength; $n++) {
+        for ($n = 0; $n < $plaintextLength; $n++) {
             $this->i = ($this->i + 1) % 256;
             $this->j = ($this->j + $this->S[$this->i]) % 256;
             list($this->S[$this->i], $this->S[$this->j]) = array(
@@ -47,13 +48,13 @@ class RC4
                 $this->S[$this->i]
             );
             $K = $this->S[($this->S[$this->i] + $this->S[$this->j]) % 256];
-            $cipher .= chr(ord($data[$n]) ^ $K);
+            $ciphertext .= chr(ord($plaintext[$n]) ^ $K);
         }
-        return $cipher;
+        return $ciphertext;
     }
 
-    public function decrypt($data)
+    public function decrypt($plaintext)
     {
-        return $this->encrypt($data);
+        return $this->encrypt($plaintext);
     }
 }
